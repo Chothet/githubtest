3278bbc8e4adb4210e3637bf7b22193bc3abce64
Chothet <chothet707@gmail.com>
2021-10-30 05:29:45 -0700

    ! added

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 	$uri .= $_SERVER['HTTP_HOST'];
 	header('Location: '.$uri.'/dashboard/');
 	exit;
-<h1> Hello World </h1>
+<h1> Hello World! </h1>
 ?>
 Something is wrong with the XAMPP installation :-(
